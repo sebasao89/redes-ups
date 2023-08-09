95bac5f64c9643e22e973cb6a23ac5824f1612d8
sebas-meseta <sebas.ao@outlook.com>
2023-08-09 17:11:50 -0500

    path de rutas establecidas en auth, service-orders y el loadchildren en el routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: 'service-orders',
+    loadChildren: () => import('./service-orders/service-orders.module').then( m => m.ServiceOrdersModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'service-orders',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 
-const routes: Routes = [];
+// localhost:4200/auth/
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutPagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
