6a528aad649badf4c2fbb3df4894ba8cc234471c
sebas-meseta <sebas.ao@outlook.com>
2023-08-09 17:28:37 -0500

    ruta children implementada en service-orders

--- src/app/service-orders/service-orders-routing.module.ts
+++ src/app/service-orders/service-orders-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { NewOrderPageComponent } from './pages/new-order-page/new-order-page.component';
 
 // localhost:4200/service-orders/
 const routes: Routes = [
   {
     path: '',
-    component: LayoutPagesComponent
+    component: LayoutPagesComponent,
+    children: [
+      {
+        path: 'new-order',
+        component: NewOrderPageComponent
+      }
+    ]
   }
 ];
 
