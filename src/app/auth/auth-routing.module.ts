27d7f4f0a81f904a4558a18c3a6b6a9f70da810b
sebas-meseta <sebas.ao@outlook.com>
2023-08-12 11:23:18 -0500

    rutas children de auth y service order, componentes de service order

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
 
 // localhost:4200/auth/
 const routes: Routes = [
   {
     path:'',
-    component: LayoutPagesComponent
+    component: LayoutPagesComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginPagesComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login' 
+      }
+    ]
   }
 ];
 
--- src/app/service-orders/service-orders-routing.module.ts
+++ src/app/service-orders/service-orders-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { NewOrderPageComponent } from './pages/new-order-page/new-order-page.component';
+import { ListOrderPageComponent } from './pages/list-order-page/list-order-page.component';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
 
 // localhost:4200/service-orders/
 const routes: Routes = [
@@ -12,6 +14,22 @@ const routes: Routes = [
       {
         path: 'new-order',
         component: NewOrderPageComponent
+      },
+      {
+        path: 'list-order',
+        component: ListOrderPageComponent
+      },
+      {
+        path: 'edit-order/:id',
+        component: NewOrderPageComponent
+      },      
+      {
+        path: ':id',
+        component: OrderPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list-order'
       }
     ]
   }
