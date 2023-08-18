919e69a4ab891672509cb315b0a9fb204309d7b7
sebas-meseta <sebas.ao@outlook.com>
2023-08-18 15:22:00 -0500

    layot auth modificado y se adiciona iconos de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
