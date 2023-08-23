d598b310c41d2d71609a83d52b815a77839c1b40
Sebasao <sebas.ao@outlook.com>
2023-08-22 21:58:26 -0500

    Proyecto redes-ups de firebase configurado con angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'redes-ups',
+    appId: '1:612939147379:web:09a9ee316d7abe80e5a206',
+    storageBucket: 'redes-ups.appspot.com',
+    apiKey: 'AIzaSyAFkdh-uqbKKDg_yeDnj9OdILn--uKFbrQ',
+    authDomain: 'redes-ups.firebaseapp.com',
+    messagingSenderId: '612939147379',
+  },};
