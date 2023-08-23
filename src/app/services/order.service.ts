6cd37377bb23546a860fdf45f06591f080251e81
sebas-meseta <sebas.ao@outlook.com>
2023-08-23 17:00:50 -0500

    componente navbar adicionado

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Order } from '../interfaces/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor( private firestore: Firestore) { }
+
+  addOrder( order: Order) {
+    const orderRef = collection(this.firestore,'orders')
+    return addDoc(orderRef, order)
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    Error404PageComponent
+    Error404PageComponent,
+    NavbarComponent
   ],
   imports: [
     
