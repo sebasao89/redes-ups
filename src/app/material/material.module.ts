2eeee284a47be5df613eba52eadd47e7d87238d1
sebas-meseta <sebas.ao@outlook.com>
2023-09-05 17:36:31 -0500

    formulario de nueva empresa iniciado

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import { MatTableModule } from '@angular/material/table';
     MatTooltipModule,
     MatListModule,
     MatTableModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
