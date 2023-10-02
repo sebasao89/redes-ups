a51f7b8913211c463b1b36d842e0caeb20590f62
sebas-meseta <sebas.ao@outlook.com>
2023-10-02 17:23:45 -0500

    implementando servicio con getEmpresas e inyectandolo en list-empresa-page

--- src/app/empresa/pages/list-empresa-page/list-empresa-page.component.ts
+++ src/app/empresa/pages/list-empresa-page/list-empresa-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmpresaService } from 'src/app/services/empresa.service';
 
 @Component({
   selector: 'app-list-empresa-page',
   templateUrl: './list-empresa-page.component.html',
   styleUrls: ['./list-empresa-page.component.scss']
 })
-export class ListEmpresaPageComponent {
+export class ListEmpresaPageComponent implements OnInit {
+
+  constructor( private empresasService: EmpresaService) {}
+
+  ngOnInit(): void {
+    this.empresasService.getEmpresas().subscribe( empresas => {
+      console.log(empresas)
+    
+    })
+  }
 
 }
--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import Empresa from '../interfaces/empresa.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +10,15 @@ export class EmpresaService {
 
   // Inyectar el servicio de firestore
   constructor( private firestore: Firestore) { }
+
+  getEmpresas():Observable<Empresa[]> {
+    const empresaRef = collection(this.firestore, 'empresas')
+    return collectionData(empresaRef, { idField: 'id' }) as Observable<Empresa[]>
+  }
+
+  addEmpresas( empresa: Empresa ) {
+    const empresaRef = collection(this.firestore, 'empresas')
+    return addDoc(empresaRef, empresa)
+  }
+
 }
