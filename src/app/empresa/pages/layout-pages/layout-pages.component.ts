3858031be9da87b504306f3f1eafd5d6287aed1d
sebas-meseta <sebas.ao@outlook.com>
2023-11-09 11:58:55 -0500

    matSnackBarModule importado en layout empresas para solucionar error de los botones

--- src/app/empresa/pages/layout-pages/layout-pages.component.ts
+++ src/app/empresa/pages/layout-pages/layout-pages.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-layout-pages',
     templateUrl: './layout-pages.component.html',
     styleUrls: ['./layout-pages.component.scss'],
     standalone: true,
-    imports: [NavbarComponent, MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterOutlet]
+    imports: [NavbarComponent, MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterOutlet, MatSnackBarModule]
 })
 export class LayoutPagesComponent {
 
